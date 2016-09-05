ecbf158dbc249afbc3b8bef4e663a475d6e81a53
ShmurX <225057938+shmurx@users.noreply.github.com>
2016-09-05 10:48:36 +0200

    Add Bundler gem config

--- install.sh
+++ install.sh
@@ -1,17 +1,20 @@
 #!/usr/bin/env bash
 
-echo "symlinking nvim config..."
+echo "symlink nvim config..."
 rm -rf ~/.config/nvim
 ln -s ~/Code/dotfiles/.config/nvim/ ~/.config/
 
-echo "setup your git config"
+echo "setup your git config..."
 rm ~/.gitconfig
 cp .gitconfig ~/.gitconfig
 
-echo "setup your URxvt config"
+echo "setup your URxvt config..."
 rm ~/.Xdefaults
 cp .Xdefaults ~/.Xdefaults
 
-echo "setup tmux"
+echo "setup tmux..."
 rm ~/.tmux.conf
 cp .tmux.conf ~/.tmux.conf
+
+echo "Improve Bundle installs..."
+cp .gemrc ~/.gemrc
